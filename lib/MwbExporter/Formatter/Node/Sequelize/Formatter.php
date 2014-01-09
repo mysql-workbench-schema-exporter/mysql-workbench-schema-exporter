862cf568f21ce73eef126771cc0c70021840ee2e
Joris Aerts <jaerts@teslamotors.com>
2014-01-09 12:59:25 -0800

    Very sketchy draft for Sequelize formatter

---
+++ lib/MwbExporter/Formatter/Node/Sequelize/Formatter.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2012 Allan Sun <sunajia@gmail.com>
+ * Copyright (c) 2012-2013 Toha <tohenk@yahoo.com>
+ * Copyright (c) 2013 WitteStier <development@wittestier.nl>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace MwbExporter\Formatter\Node\Sequelize;
+
+use MwbExporter\Formatter\Node\Formatter as BaseFormatter;
+use MwbExporter\Model\Base;
+
+class Formatter extends BaseFormatter
+{
+    const CFG_GENERATE_VALIDATION    = 'generateValidation';
+    const CFG_GENERATE_PROXY         = 'generateProxy';
+
+    /**
+     * (non-PHPdoc)
+     * @see \MwbExporter\Formatter::init()
+     */
+    protected function init()
+    {
+        parent::init();
+        $this->addConfigurations(array(
+            static::CFG_FILENAME             => '%entity%.%extension%',
+            static::CFG_GENERATE_VALIDATION  => true,
+            static::CFG_GENERATE_PROXY       => true,
+        ));
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \MwbExporter\Formatter::createTable()
+     */
+    public function createTable(Base $parent, $node)
+    {
+        return new Model\Table($parent, $node);
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \MwbExporter\FormatterInterface::getTitle()
+     */
+    public function getTitle()
+    {
+        return 'Node Sequelize Model';
+    }
+}
---
+++ lib/MwbExporter/Formatter/Node/Sequelize/Model/Table.php
@@ -0,0 +1,345 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2012 Allan Sun <sunajia@gmail.com>
+ * Copyright (c) 2012-2013 Toha <tohenk@yahoo.com>
+ * Copyright (c) 2013 WitteStier <development@wittestier.nl>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace MwbExporter\Formatter\Node\Sequelize\Model;
+
+use MwbExporter\Formatter\Node\Model\Table as BaseTable;
+use MwbExporter\Formatter\Node\Sequelize\Formatter;
+use MwbExporter\Writer\WriterInterface;
+
+class Table extends BaseTable
+{
+    public function writeTable(WriterInterface $writer)
+    {
+        switch (true) {
+            case $this->isExternal():
+                return self::WRITE_EXTERNAL;
+
+            case $this->isManyToMany():
+                return self::WRITE_M2M;
+
+            default:
+                $writer->open($this->getTableFileName());
+                $this->writeBody($writer);
+                $writer->close();
+                return self::WRITE_OK;
+        }
+    }
+
+    /**
+     * Write model body code.
+     * 
+     * @param \MwbExporter\Writer\WriterInterface $writer
+     * @return \MwbExporter\Formatter\Sencha\ExtJS4\Model\Table
+     */
+    public function writeBody(WriterInterface $writer)
+    {
+        $writer
+            ->write("module.exports = function(sequelize, DataTypes) {")
+            ->write("  return sequelize.define('%s', %s, %s)};", $this->getModelName(), $this->asModel(), $this->asOptions())
+            ;
+            
+        return $this;
+    }
+
+    public function asOptions()
+    {
+        $result = array(
+            'timestamps' => false,
+            'underscored' => true,
+            'tableName' => $this->getRawTableName(),
+            'syncOnAssociation' => false
+        );
+        
+        return $this->getJSObject($result);
+        /*
+        $result = array('extend' => $this->getParentClass());
+        if (count($data = $this->getUses())) {
+            $result['uses'] = $data;
+        }
+        if (count($data = $this->getBelongsTo())) {
+            $result['belongsTo'] = $data;
+        }
+        if (count($data = $this->getHasOne())) {
+            $result['hasOne'] = $data;
+        }
+        if (count($data = $this->getHasMany())) {
+            $result['hasMany'] = $data;
+        }
+        if (count($data = $this->getFields())) {
+            $result['fields'] = $data;
+        }
+        if ($this->getDocument()->getConfig()->get(Formatter::CFG_GENERATE_VALIDATION) && count($data = $this->getValidations())) {
+            $result['validations'] = $data;
+        }
+        if ($this->getDocument()->getConfig()->get(Formatter::CFG_GENERATE_PROXY) && count($data = $this->getAjaxProxy())) {
+            $result['proxy'] = $data;
+        }
+
+        return $this->getJSObject($result);*/        
+    }
+    public function asModel()
+    {
+        $result = $this->getFields();
+        
+        return str_replace("'","",$this->getJSObject($result)); // hacky
+    }
+
+    /**
+     * Get uses.
+     *
+     * @link http://docs.sencha.com/extjs/4.2.0/#!/api/Ext.Class-cfg-uses
+     * @return array
+     */
+    protected function getUses()
+    {
+        $result = array();
+        $current = sprintf('%s.%s', $this->getClassPrefix(), $this->getModelName());
+
+        // Collect belongsTo uses.
+        foreach ($this->relations as $relation) {
+            $refTableName = sprintf('%s.%s', $this->getClassPrefix(), $relation->getReferencedTable()->getModelName());
+            if ($relation->isManyToOne() && !in_array($refTableName, $result) && ($refTableName !== $current)) {
+                $result[] = $refTableName;
+            }
+        }
+
+        // Collect hasOne uses.
+        foreach ($this->relations as $relation) {
+            $refTableName = sprintf('%s.%s', $this->getClassPrefix(), $relation->getReferencedTable()->getModelName());
+            if (!$relation->isManyToOne() && !in_array($refTableName, $result) && ($refTableName !== $current)) {
+                $result[] = $refTableName;
+            }
+        }
+
+        // Collect hasMany uses.
+        foreach ($this->getManyToManyRelations() as $relation) {
+            $referencedTable = $relation['refTable'];
+            $refTableName = sprintf('%s.%s', $this->getClassPrefix(), $referencedTable->getModelName());
+            if (!in_array($refTableName, $result) && ($refTableName !== $current)) {
+                $result[] = $refTableName;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get BelongsTo relations.
+     *
+     * @link http://docs.sencha.com/extjs/4.2.0/#!/api/Ext.data.association.BelongsTo
+     * @return array
+     */
+    protected function getBelongsTo()
+    {
+        $result = array();
+        foreach ($this->getRelations() as $relation) {
+            if (!$relation->isManyToOne()) {
+                // Do not list OneToOne relations.
+                continue;
+            }
+            $referencedTable = $relation->getReferencedTable();
+            $result[] = array(
+                'model'          => sprintf('%s.%s', $this->getClassPrefix(), $referencedTable->getModelName()),
+                'associationKey' => lcfirst($referencedTable->getModelName()),
+                'getterName'     => sprintf('get%s', $referencedTable->getModelName()),
+                'setterName'     => sprintf('set%s', $referencedTable->getModelName()),
+            );
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get HasOne relations.
+     *
+     * @link http://docs.sencha.com/extjs/4.2.0/#!/api/Ext.data.association.HasOne
+     * @return array
+     */
+    protected function getHasOne()
+    {
+        $result = array();
+        foreach ($this->getRelations() as $relation) {
+            if ($relation->isManyToOne()) {
+                // Do not list manyToOne relations.
+                continue;
+            }
+            $referencedTable = $relation->getReferencedTable();
+            $result[] = array(
+                'model'          => sprintf('%s.%s', $this->getClassPrefix(), $referencedTable->getModelName()),
+                'associationKey' => lcfirst($referencedTable->getModelName()),
+                'getterName'     => sprintf('get%s', $referencedTable->getModelName()),
+                'setterName'     => sprintf('set%s', $referencedTable->getModelName()),
+            );
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get HasMany relations.
+     *
+     * @link http://docs.sencha.com/extjs/4.2.0/#!/api/Ext.data.association.HasMany
+     * @return array
+     */
+    protected function getHasMany()
+    {
+        $result = array();
+        foreach ($this->getManyToManyRelations() as $relation) {
+            $referencedTable = $relation['refTable'];
+            $result[] = array(
+                'model'          => sprintf('%s.%s', $this->getClassPrefix(), $referencedTable->getModelName()),
+                'associationKey' => lcfirst($referencedTable->getModelName()),
+                'name'           => sprintf('get%sStore', $referencedTable->getModelName()),
+            );
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get model fields.
+     *
+     * @return array
+     */
+    protected function getFields()
+    {
+        $result = array();
+        foreach ($this->getColumns() as $column) 
+        {
+            $type = $this->getDocument()->getFormatter()->getDatatypeConverter()->getType($column);
+            $c = array();
+            $c['type'] = 'DataTypes.' . ($type ? $type : 'STRING.BINARY');
+            
+            if($column->isAutoIncrement()) {
+                $c['autoIncrement'] = true;
+            }
+            if($column->isPrimary()) {
+                $c['primaryKey'] = true;
+            }
+            $result[$column->getColumnName()] = $c;
+            //$result[strtolower($column->getColumnName())] = $c;
+            //$result[strtolower($column->getColumnName())] = $c;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get model field validations.
+     *
+     * @return array
+     */
+    protected function getValidations()
+    {
+        $result = array();
+        foreach ($this->getColumns() as $column) {
+            if ($column->isNotNull() && !$column->isPrimary()) {
+                $result[] = array(
+                    'type'  => 'presence',
+                    'field' => $column->getColumnName(),
+                );
+            }
+            if (($len = $column->getLength()) > 0) {
+                $result[] = array(
+                    'type'  => 'length',
+                    'field' => $column->getColumnName(),
+                    'max'   => $len,
+                );
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get model ajax proxy object.
+     *
+     * @link http://docs.sencha.com/extjs/4.2.0/#!/api/Ext.data.proxy.Ajax
+     * @return array
+     */
+    protected function getAjaxProxy()
+    {
+        return array(
+            'type'   => 'ajax',
+            'url'    => sprintf('/data/%s', strtolower($this->getModelName())),
+            'api'    => $this->getApi(),
+            'reader' => $this->getJsonReader(),
+            'writer' => $this->getJsonWriter(),
+        );
+    }
+
+    /**
+     * Get the model API object.
+     *
+     * @link http://docs.sencha.com/extjs/4.2.0/#!/api/Ext.data.proxy.Ajax-cfg-api
+     * @return array
+     */
+    private function getApi()
+    {
+        $modelName = strtolower($this->getModelName());
+
+        return array(
+            'read'    => sprintf('/data/%s', $modelName),
+            'update'  => sprintf('/data/%s/update', $modelName),
+            'create'  => sprintf('/data/%s/add', $modelName),
+            'destroy' => sprintf('/data/%s/destroy', $modelName),
+        );
+    }
+
+    /**
+     * Get the model json reader.
+     *
+     * @link http://docs.sencha.com/extjs/4.2.0/#!/api/Ext.data.reader.Json
+     * @return array
+     */
+    private function getJsonReader()
+    {
+        return array(
+            'type'            => 'json',
+            'root'            => strtolower($this->getModelName()),
+            'messageProperty' => 'message',
+        );
+    }
+
+    /**
+     * Get the model json writer.
+     *
+     * @link http://docs.sencha.com/extjs/4.2.0/#!/api/Ext.data.writer.Json
+     * @return array
+     */
+    private function getJsonWriter()
+    {
+        return array(
+            'type'       => 'json',
+            'root'       => strtolower($this->getModelName()),
+            'encode'     => true,
+            'expandData' => true,
+        );
+    }
+}
