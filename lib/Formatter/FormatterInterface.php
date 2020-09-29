e1ea003f5ab87a30330044cf5a49768498bee8b1
Toha <tohenk@yahoo.com>
2020-09-29 15:41:58 +0700

    Bump next dev version.
    Signed-off-by: Toha <tohenk@yahoo.com>

--- lib/Formatter/FormatterInterface.php
+++ lib/Formatter/FormatterInterface.php
@@ -31,7 +31,7 @@ use MwbExporter\Model\Base;
 
 interface FormatterInterface {
 
-    const VERSION                           = '3.1.3-dev';
+    const VERSION                           = '3.1.4-dev';
 
     const CFG_LANGUAGE                      = 'language';
     const CFG_NAMING_STRATEGY               = 'namingStrategy';
