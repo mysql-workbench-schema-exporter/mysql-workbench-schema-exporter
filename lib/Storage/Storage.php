fb91d5a282c5edb2bd18152c1ed7dbea8fec0338
Toha <tohenk@yahoo.com>
2024-09-22 05:32:41 +0700

    Update copyright year.
    Signed-off-by: Toha <tohenk@yahoo.com>

--- lib/Storage/Storage.php
+++ lib/Storage/Storage.php
@@ -4,7 +4,7 @@
  * The MIT License
  *
  * Copyright (c) 2010 Johannes Mueller <circus2(at)web.de>
- * Copyright (c) 2012-2023 Toha <tohenk@yahoo.com>
+ * Copyright (c) 2012-2024 Toha <tohenk@yahoo.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
